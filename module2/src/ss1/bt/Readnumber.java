e4f7cd502e96e0ce26730881eb3399548517bfc9
thorbeuse <103766473+thorbeuse@users.noreply.github.com>
2022-09-07 18:45:29 +0700

    1

---
+++ module2/src/ss1/bt/Readnumber.java
@@ -0,0 +1,7 @@
+package ss1.bt;
+
+public class Readnumber {
+    public static void main(String[] args) {
+        String donvi 
+    }
+}
---
+++ module2/src/ss1/bt/hello.java
@@ -0,0 +1,13 @@
+package ss1.bt;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter name: ");
+        String name = scanner.nextLine();
+        System.out.println("hello:" + name);
+    }
+}
+h
