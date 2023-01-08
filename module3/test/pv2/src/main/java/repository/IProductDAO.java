96fa6b7e8c8efe47fffd05e885da1e81ca622aef
thorbeuse <103766473+thorbeuse@users.noreply.github.com>
2023-01-09 06:26:57 +0700

    chua phan trang chua modal

---
+++ module3/test/pv2/src/main/java/repository/IProductDAO.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.Product;
+
+import java.util.List;
+
+public interface IProductDAO extends GeneralService<Product> {
+   public List<Product> findByName(String productName);
+
+}
